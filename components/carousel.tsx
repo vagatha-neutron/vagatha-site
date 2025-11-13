de18705cd8389dd5f09b60f38b16f4f1e89d2db1
vagatha-neutron <vagatha.neutron@gmail.com>
2025-11-13 13:39:21 -0600

    “Cambio_de_componente”

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,62 +1,32 @@
 "use client";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, FreeMode } from "swiper/modules";
 import Image from "next/image";
 
-export default function Carrusel() {
-  const images = [
-    "/images/manos_usando_joyeria_vagatha.jpg",
-    "/images/manos_modelando_anillos_vagatha.jpg",
-    "/images/mostrando_cadena_y_joyeria_vagatha.jpg",
-    "/images/guardando_en_el_bolsillo_joyeria_vagatha.jpg",
-    "/images/modelo_viendose_en_el_espejo_joyeria_vagatha.jpg",
-  ];
+const images = [
+  "/images/manos_usando_joyeria_vagatha.jpg",
+  "/images/manos_modelando_anillos_vagatha.jpg",
+  "/images/mostrando_cadena_y_joyeria_vagatha.jpg",
+  "/images/guardando_en_el_bolsillo_joyeria_vagatha.jpg",
+  "/images/modelo_viendose_en_el_espejo_joyeria_vagatha.jpg",
+];
 
-  const extended = [...images, ...images];
+export default function Carrusel() {
+  const loopImages = [...images, ...images]; // duplicamos
 
   return (
-    <section className="w-full bg-beige">
-      <Swiper
-        modules={[Autoplay, FreeMode]}
-        loop={true}
-        freeMode={{
-          enabled: true,
-          momentum: false,
-        }}
-        slidesPerView="auto"
-        spaceBetween={10}
-        speed={6000}
-        allowTouchMove={false}
-        touchRatio={0}
-        simulateTouch={false}
-        touchStartPreventDefault={false}
-        autoplay={{
-          delay: 0,
-          disableOnInteraction: false,
-          stopOnLastSlide: false,
-          pauseOnMouseEnter: false,
-        }}
-        className="flex w-full h-[485px]"
-      >
-        {extended.map((src, i) => (
-          <SwiperSlide
-            key={i}
-            className="!w-auto flex-shrink-0 h-[485px] overflow-hidden"
-          >
-            <div className="relative h-[485px] w-[350px]">
-              <Image
-                src={src}
-                alt={`Vágatha carrusel ${i + 1}`}
-                fill
-                priority={false}
-                loading="lazy"
-                sizes="350px"
-                className="object-cover"
-              />
-            </div>
-          </SwiperSlide>
+    <section className="bg-beige w-full overflow-hidden">
+      <div className="slider-track">
+        {loopImages.map((src, i) => (
+          <div key={i} className="slide">
+            <Image
+              src={src}
+              alt={`Vágatha carrusel ${i}`}
+              fill
+              sizes="350px"
+              className="object-cover"
+            />
+          </div>
         ))}
-      </Swiper>
+      </div>
     </section>
   );
 }
